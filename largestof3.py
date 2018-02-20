ab435f15772c9273bc022a5fe1935bbab2b0b2bb
imnithu <36627076+imnithu@users.noreply.github.com>
2018-02-20 17:18:20 +0530

    Create largestof3.py

---
+++ largestof3.py
@@ -0,0 +1,10 @@
+a=int(input("Enter first number"))
+b=int(input("Enter second number"))
+c=int(input("Enter third number"))
+if(a>c and a>c):
+	print("First Number is the greatest")
+elif(b>c and b>a):
+	print("Second Number is the greatest")
+else:
+	print("Third Number is the greatest")
+    
