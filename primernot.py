604069c6e79d678e26fc1844586cbb2613c42947
Nithyanandhan <36627076+imnithu@users.noreply.github.com>
2018-03-13 18:01:47 +0530

    Create primernot.py

---
+++ primernot.py
@@ -0,0 +1,13 @@
+n=int(input("Enter a number to check"))
+if(n==2):
+  print("It is a Prime number")
+elif n > 2:
+  for i in range(2,n):
+    if (n % i) == 0:
+      print("It is not a prime number")
+      break
+    else:
+      print("It is a prime number")
+      break
+else:
+  print("It is not a prime number")
