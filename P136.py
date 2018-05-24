00d89a1ac8d3704106f215c40660ab820cf9ef7f
Nithyanandhan <36627076+imnithu@users.noreply.github.com>
2018-05-24 12:20:48 +0530

    Create P136.py

---
+++ P136.py
@@ -0,0 +1,10 @@
+b=int(input("Enter the array size"))
+a=[]
+for i in range(0,b):
+  c=(input("Enter the number"))
+  a.append(c)
+k=input("Enter the Number to be Checked")
+if k in a:
+  print("yes", a.count(k))
+else:
+  print("no")
