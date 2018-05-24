ae0359063f0cfcd289893857332b2e2c437b2447
Nithyanandhan <36627076+imnithu@users.noreply.github.com>
2018-05-24 11:59:48 +0530

    Update P135.py

--- P135.py
+++ P135.py
@@ -1,6 +1,7 @@
-a=list(input("Enter the numbers"))
+a=list(input("Enter any list of numbers"))
 b=a[:len(a)//2]
 c=a[len(a)//2:]
 b.sort()
-print('Ascending Order of First Half:',b)
-print('Descending Order of Second Half:',sorted(c,reverse=True))
+d=sorted(c,reverse=True)
+e=b+d
+print("".join(e))
